64d322c4d0c6d4fd3bd18496ef2cfc9c1766af94
Tom Diethe <tdiethe@gmail.com>
2016-04-08 23:31:03 +0100

    Still not managed to get date ordering working ...

--- assets/js/csv_to_html_table.js
+++ assets/js/csv_to_html_table.js
@@ -2,12 +2,15 @@ function init_table(options) {
 
   options = options || {};
   var csv_path = options.csv_path || "";
-  var el = options.element || "table-container";
+  var el = $("#" + options.element || "table-container");
   var allow_download = options.allow_download || false;
   var csv_options = options.csv_options || {};
   var datatables_options = options.datatables_options || {};
 
-  $("#" + el).html("<table class='table table-striped table-condensed' id='my-table'></table>");
+  el.html("<table class='table table-striped table-condensed' id='my-table' data-order='[[ 1, &quot;desc&quot; ]]'></table>");
+
+  var table = $('#my-table');
+
 
   $.when($.get(csv_path)).then(
     function(data){      
@@ -15,28 +18,34 @@ function init_table(options) {
 
       var table_head = "<thead><tr>";
 
-      for (head_id = 0; head_id < csv_data[0].length; head_id++) { 
+      for (var head_id = 0; head_id < csv_data[0].length; head_id++) {
         table_head += "<th>" + csv_data[0][head_id] + "</th>";
       }
 
       table_head += "</tr></thead>";
-      $('#my-table').append(table_head);
-      $('#my-table').append("<tbody></tbody>");
+      table.append(table_head);
+      table.append("<tbody></tbody>");
 
-      for (row_id = 1; row_id < csv_data.length; row_id++) { 
+      for (var row_id = 1; row_id < csv_data.length; row_id++) {
         var row_html = "<tr>";
 
-          for (col_id = 0; col_id < csv_data[row_id].length; col_id++) { 
-            row_html += "<td>" + csv_data[row_id][col_id] + "</td>";
+          for (var col_id = 0; col_id < csv_data[row_id].length; col_id++) {
+              var cell = csv_data[row_id][col_id];
+              var order = "";
+              if (col_id == 0) {
+                  // data = new Date(cell);
+                  order =  " data-order='" + new Date(cell).toISOString() + "'";
+              }
+            row_html += "<td" + order + ">" + cell + "</td>";
           }
           
         row_html += "</tr>";
-        $('#my-table tbody').append(row_html);
+        table.find('tbody').append(row_html);
       }
 
-      $("#my-table").dataTable(datatables_options);
+        table.dataTable(datatables_options);
 
       if (allow_download)
-        $("#" + el).append("<p><a class='btn btn-info' href='" + csv_path + "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>");
+        el.append("<p><a class='btn btn-info' href='" + csv_path + "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>");
     });
 }
